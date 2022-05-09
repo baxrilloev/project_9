2d285edf66beb6da6898463105db25886e9b19b7
Badriddin <frozwshkh@gmail.com>
2022-05-09 11:50:01 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// let a = +prompt('a')
+// let method = prompt('method')
+// let b = +prompt('b')
+
+
+// switch (method) {
+//     case '+':
+//         console.log(a + b);
+//         break;
+//     case '-':
+//         console.log(a - b);
+//             break;
+//     case '+':
+//         console.log(a * b);
+//             break;
+//     case '/':
+//         console.log(a / b);
+//             break;
+
+// }
+
+
+
+// let arr_names = ['Alex', 'George', 'Michael']
+
+// let max_name = ''
+// let arr = []
+
+// arr_names.filter(item => {
+//     if(item.length > max_name.length) {
+//         max_name = item
+//     }
+// })
+
+
+// console.log(max_name);
